7fb34877a4d846e07cd4261feb2bed0f52fce87e
Winnandarkyaw <winnandarkyaw2000@gmail.com>
2024-09-09 21:05:09 +0630

    errorfix

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default class ApiService {
   static BASE_URL = "http://localhost:8080";
@@ -11,6 +11,9 @@ export default class ApiService {
     };
   }
 
+  /**AUTH */
+
+  /* This  register a new user */
   static async registerUser(registration) {
     const response = await axios.post(
       `${this.BASE_URL}/auth/register`,
@@ -19,6 +22,7 @@ export default class ApiService {
     return response.data;
   }
 
+  /* This  login a registered user */
   static async loginUser(loginDetails) {
     const response = await axios.post(
       `${this.BASE_URL}/auth/login`,
@@ -27,9 +31,12 @@ export default class ApiService {
     return response.data;
   }
 
+  /***USERS */
+
+  /*  This is  to get the user profile */
   static async getAllUsers() {
     const response = await axios.get(`${this.BASE_URL}/users/all`, {
-      headers: this.getHeader(), // Correcting 'header' to 'headers'
+      headers: this.getHeader(),
     });
     return response.data;
   }
@@ -44,16 +51,18 @@ export default class ApiService {
     return response.data;
   }
 
+  /* This is the  to get a single user */
   static async getUser(userId) {
     const response = await axios.get(
       `${this.BASE_URL}/users/get-by-id/${userId}`,
       {
-        headers: this.getHeader(), // Correcting 'header' to 'headers'
+        headers: this.getHeader(),
       }
     );
     return response.data;
   }
 
+  /* This is the  to get user bookings by the user id */
   static async getUserBookings(userId) {
     const response = await axios.get(
       `${this.BASE_URL}/users/get-user-bookings/${userId}`,
@@ -64,6 +73,7 @@ export default class ApiService {
     return response.data;
   }
 
+  /* This is to delete a user */
   static async deleteUser(userId) {
     const response = await axios.delete(
       `${this.BASE_URL}/users/delete/${userId}`,
@@ -74,6 +84,8 @@ export default class ApiService {
     return response.data;
   }
 
+  /**ROOM */
+  /* This  adds a new room room to the database */
   static async addRoom(formData) {
     const result = await axios.post(`${this.BASE_URL}/rooms/add`, formData, {
       headers: {
@@ -84,6 +96,7 @@ export default class ApiService {
     return result.data;
   }
 
+  /* This  gets all availavle rooms */
   static async getAllAvailableRooms() {
     const result = await axios.get(
       `${this.BASE_URL}/rooms/all-available-rooms`
@@ -91,25 +104,30 @@ export default class ApiService {
     return result.data;
   }
 
-  static async getAvailableRoomsByDataAndType(
+  /* This  gets all availavle by dates rooms from the database with a given date and a room type */
+  static async getAvailableRoomsByDateAndType(
     checkInDate,
     checkOutDate,
     roomType
   ) {
     const result = await axios.get(
-      `${this.BASE_URL}/rooms/available-rooms-by-date-and-type`,
-      {
-        params: { checkInDate, checkOutDate, roomType },
-      }
+      `${this.BASE_URL}/rooms/available-rooms-by-date-and-type?checkInDate=${checkInDate}
+		&checkOutDate=${checkOutDate}&roomType=${roomType}`
     );
     return result.data;
   }
 
+  /* This  gets all room types from thee database */
   static async getRoomTypes() {
     const response = await axios.get(`${this.BASE_URL}/rooms/types`);
     return response.data;
   }
-
+  /* This  gets all rooms from the database */
+  static async getAllRooms() {
+    const result = await axios.get(`${this.BASE_URL}/rooms/all`);
+    return result.data;
+  }
+  /* This funcction gets a room by the id */
   static async getRoomById(roomId) {
     const result = await axios.get(
       `${this.BASE_URL}/rooms/room-by-id/${roomId}`
@@ -117,6 +135,7 @@ export default class ApiService {
     return result.data;
   }
 
+  /* This  deletes a room by the Id */
   static async deleteRoom(roomId) {
     const result = await axios.delete(
       `${this.BASE_URL}/rooms/delete/${roomId}`,
@@ -127,6 +146,7 @@ export default class ApiService {
     return result.data;
   }
 
+  /* This updates a room */
   static async updateRoom(roomId, formData) {
     const result = await axios.put(
       `${this.BASE_URL}/rooms/update/${roomId}`,
@@ -141,6 +161,22 @@ export default class ApiService {
     return result.data;
   }
 
+  /**BOOKING */
+  /* This  saves a new booking to the databse */
+  static async bookRoom(roomId, userId, booking) {
+    console.log("USER ID IS: " + userId);
+
+    const response = await axios.post(
+      `${this.BASE_URL}/bookings/book-room/${roomId}/${userId}`,
+      booking,
+      {
+        headers: this.getHeader(),
+      }
+    );
+    return response.data;
+  }
+
+  /* This  gets alll bokings from the database */
   static async getAllBookings() {
     const result = await axios.get(`${this.BASE_URL}/bookings/all`, {
       headers: this.getHeader(),
@@ -148,6 +184,7 @@ export default class ApiService {
     return result.data;
   }
 
+  /* This  get booking by the cnfirmation code */
   static async getBookingByConfirmationCode(bookingCode) {
     const result = await axios.get(
       `${this.BASE_URL}/bookings/get-by-confirmation-code/${bookingCode}`
@@ -155,6 +192,7 @@ export default class ApiService {
     return result.data;
   }
 
+  /* This is the  to cancel user booking */
   static async cancelBooking(bookingId) {
     const result = await axios.delete(
       `${this.BASE_URL}/bookings/cancel/${bookingId}`,
@@ -162,9 +200,10 @@ export default class ApiService {
         headers: this.getHeader(),
       }
     );
-    return result.data; // Fixed 'result.date' to 'result.data'
+    return result.data;
   }
 
+  /**AUTHENTICATION CHECKER */
   static logout() {
     localStorage.removeItem("token");
     localStorage.removeItem("role");
@@ -185,3 +224,4 @@ export default class ApiService {
     return role === "USER";
   }
 }
+// export default new ApiService();
