21a77f5c525d10d1f681478a980c2d335482e9c2
Win Nandar Kyaw <74086347+Winnandarkyaw@users.noreply.github.com>
2024-09-09 21:14:41 +0630

    Update UserController.java
    usercors

--- src/main/java/com/winy/WinyHotel/controller/UserController.java
+++ src/main/java/com/winy/WinyHotel/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+
 @RequestMapping("/users")
 public class UserController {
     @Autowired
