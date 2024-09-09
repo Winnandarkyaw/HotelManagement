33e0d04a66fc4c8e39a3a617fa365f0425de24f7
Winnandarkyaw <winnandarkyaw2000@gmail.com>
2024-09-09 20:35:28 +0630

    frontendandbackend

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class ApiService {
-  static BASE_URL = "https://localhost:8080";
+  static BASE_URL = "http://localhost:8080";
+
   static getHeader() {
     const token = localStorage.getItem("token");
     return {
@@ -10,151 +11,177 @@ export default class ApiService {
     };
   }
 
-  static async registerUser(registration)
-  {
-    const response=await axios.post(`${this.BASE_URL}/auth/register`,registration)
-    return response.data
+  static async registerUser(registration) {
+    const response = await axios.post(
+      `${this.BASE_URL}/auth/register`,
+      registration
+    );
+    return response.data;
   }
 
-  static async loginUser(loginDetails)
-  {
-    const response=await axios.post(`${this.BASE_URL}/auth/login`,loginDetails)
-    return response.data
-  }
-  
-  static async getAllUsers()
-  {
-    const response=await axios.get(`${this.BASE_URL}/users/all`,{
-        header:this.getHeader()
-    })
-    return response.data
-  }
-  static async getUserProfile()
-  {
-    const response=await axios.get(`${this.BASE_URL}/users/get-logged-in-profile-info`,{
-      headers:this.getHeader()
-    })
-    return response.data
+  static async loginUser(loginDetails) {
+    const response = await axios.post(
+      `${this.BASE_URL}/auth/login`,
+      loginDetails
+    );
+    return response.data;
   }
 
-  static async getUser(userId)
-  {
-    const response=await axios.get(`${this.BASE_URL}/users/get-by-id/${userId}`,{
-      header:this.getHeader()
-    })
-    return response.data
+  static async getAllUsers() {
+    const response = await axios.get(`${this.BASE_URL}/users/all`, {
+      headers: this.getHeader(), // Correcting 'header' to 'headers'
+    });
+    return response.data;
   }
 
-  static async getUserBookings(userId)
-  {
-    const response=await axios.get(`${this.BASE_URL}/users/get-user-bookings/${userId}`,{
-      headers:this.getHeader()
-    })
-    return response.data
-  }
-
-  static async deleteUser(userId)
-  {
-    const response = await axios.delete(`${this.BASE_URL}/users/delete/${userId}`,{
-      headers:this.getHeader()
-    })
-    return response.data
-  }
-  
-  static async addRoom(formData)
-  {
-    const result=await axios.post(`${this.BASE_URL}/rooms/add`,formData,{
-      headers:{
-        ...this.getHeader(),
-        'Content-Type':'multipart/form-data'
+  static async getUserProfile() {
+    const response = await axios.get(
+      `${this.BASE_URL}/users/get-logged-in-profile-info`,
+      {
+        headers: this.getHeader(),
       }
+    );
+    return response.data;
+  }
+
+  static async getUser(userId) {
+    const response = await axios.get(
+      `${this.BASE_URL}/users/get-by-id/${userId}`,
+      {
+        headers: this.getHeader(), // Correcting 'header' to 'headers'
+      }
+    );
+    return response.data;
+  }
+
+  static async getUserBookings(userId) {
+    const response = await axios.get(
+      `${this.BASE_URL}/users/get-user-bookings/${userId}`,
+      {
+        headers: this.getHeader(),
+      }
+    );
+    return response.data;
+  }
+
+  static async deleteUser(userId) {
+    const response = await axios.delete(
+      `${this.BASE_URL}/users/delete/${userId}`,
+      {
+        headers: this.getHeader(),
+      }
+    );
+    return response.data;
+  }
+
+  static async addRoom(formData) {
+    const result = await axios.post(`${this.BASE_URL}/rooms/add`, formData, {
+      headers: {
+        ...this.getHeader(),
+        "Content-Type": "multipart/form-data",
+      },
     });
     return result.data;
   }
 
-  static async getAllAvailableRooms()
-  {
-    const result=await axios.get(`${this.BASE_URL}/rooms/all-available-rooms`)
-    return result.data
-  }
-  
-  static async getAvailableRoomsByDataAndType(checkInDate,checkOutDate,roomType)
-  {
-    const result=await axios.get(`${this.BASE_URL}/rooms/available-rooms-by-date-and-type?checkInDate=${checkInDate}&checkOutDate=${checkOutDate}&roomType=${roomType}`)
+  static async getAllAvailableRooms() {
+    const result = await axios.get(
+      `${this.BASE_URL}/rooms/all-available-rooms`
+    );
     return result.data;
   }
-  
-  static async getRoomTypes()
-  {
-    const response=await axios.get(`${this.BASE_URL}/rooms/types`)
-    return response.data
-  }
-  static async getRoomById(roomId)
-  {
-    const result=await axios.get(`${this.BASE_URL}/rooms/room-by-id/${roomId}`)
-    return result.data
-  }
-  static async deleteRoom(roomId)
-  {
-    const result=await axios.delete(`${this.BASE_URL}/rooms/delete/${roomId}`,{
-      headers:this.getHeader()
-    })
-    return result.data
-  }
 
-  static async updateRoom(roomId,formData)
-  {
-    const result=await axios.put(`${this.BASE_URL}/rooms/update/${roomId}`,formData,{
-      header:{
-        ...this.getHeader(),
-        'Content-Type':'multipart/form-data'
+  static async getAvailableRoomsByDataAndType(
+    checkInDate,
+    checkOutDate,
+    roomType
+  ) {
+    const result = await axios.get(
+      `${this.BASE_URL}/rooms/available-rooms-by-date-and-type`,
+      {
+        params: { checkInDate, checkOutDate, roomType },
       }
+    );
+    return result.data;
+  }
+
+  static async getRoomTypes() {
+    const response = await axios.get(`${this.BASE_URL}/rooms/types`);
+    return response.data;
+  }
+
+  static async getRoomById(roomId) {
+    const result = await axios.get(
+      `${this.BASE_URL}/rooms/room-by-id/${roomId}`
+    );
+    return result.data;
+  }
+
+  static async deleteRoom(roomId) {
+    const result = await axios.delete(
+      `${this.BASE_URL}/rooms/delete/${roomId}`,
+      {
+        headers: this.getHeader(),
+      }
+    );
+    return result.data;
+  }
+
+  static async updateRoom(roomId, formData) {
+    const result = await axios.put(
+      `${this.BASE_URL}/rooms/update/${roomId}`,
+      formData,
+      {
+        headers: {
+          ...this.getHeader(),
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return result.data;
+  }
+
+  static async getAllBookings() {
+    const result = await axios.get(`${this.BASE_URL}/bookings/all`, {
+      headers: this.getHeader(),
     });
-    return result.data
+    return result.data;
   }
 
-  static async getAllBookings()
-  {
-    const result=await axios.get(`${this.BASE_URL}/bookings/all`,{
-      headers:this.getHeader()
-    });
-    return result.data
+  static async getBookingByConfirmationCode(bookingCode) {
+    const result = await axios.get(
+      `${this.BASE_URL}/bookings/get-by-confirmation-code/${bookingCode}`
+    );
+    return result.data;
   }
 
-  static async getBookingByConfirmationCode(bookingCode)
-  {
-    const result=await axios.get(`${this.BASE_URL}/bookings/get-by-confirmation-code/${bookingCode}`)
-    return result.data
+  static async cancelBooking(bookingId) {
+    const result = await axios.delete(
+      `${this.BASE_URL}/bookings/cancel/${bookingId}`,
+      {
+        headers: this.getHeader(),
+      }
+    );
+    return result.data; // Fixed 'result.date' to 'result.data'
   }
 
-  static async cancelBooking(bookingId)
-  {
-    const result=await axios.delete(`${this.BASE_URL}/bookings/cancel/${bookingId}`,{
-      headers:this.getHeader()
-    })
-    return result.date
+  static logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
   }
 
-  static logout()
-  {
-    localStorage.removeItem('token')
-    localStorage.removeItem('role')
-  }
-  static isAuthenticated()
-  {
-    const token=localStorage.getItem('token')
-    return !!token
+  static isAuthenticated() {
+    const token = localStorage.getItem("token");
+    return !!token;
   }
 
-  static isAdmin()
-  {
-    const role=localStorage.getItem('role')
-    return role === 'ADMIN'
+  static isAdmin() {
+    const role = localStorage.getItem("role");
+    return role === "ADMIN";
   }
 
-  static isUser()
-  {
-    const role=localStorage.getItem('role')
-    return role==='USER'
+  static isUser() {
+    const role = localStorage.getItem("role");
+    return role === "USER";
   }
 }
